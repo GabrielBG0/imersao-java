66707e37e2063d965853161f694ecd0866579003
Gabriel B Gutierrez <gabriel.bgs00@gmail.com>
2022-07-18 17:31:35 -0400

    dia 1

---
+++ alura-stickers/src/App.java
@@ -0,0 +1,36 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // fazer uma conexão http e buscar os top 250 filmes
+
+        String url = "https://imdb-api.com/en/API/Top250Movies/k_tum8g9d7";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+
+        HttpResponse<String> respose = client.send(request, BodyHandlers.ofString());
+
+        String body = respose.body();
+
+        // pegar apenas dados que nos interessão (titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        // exibir e manipular dados
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+
+    }
+}
